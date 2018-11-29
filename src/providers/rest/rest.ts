595e67d94a7466a699cc1b60a42d22ff1b7df947
valideo <valentin.bredy@hotmail.fr>
2018-11-29 15:03:47 +0100

    created api functions

--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -1,17 +1,64 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the RestProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class RestProvider {
+
+  apiSearchMovieUrl = "http://www.omdbapi.com/?apikey=75522b56&type=movie&s=";
+  apiSearchSerieUrl = "http://www.omdbapi.com/?apikey=75522b56&type=series&s="
+  apiGetMediaUrl = "http://www.omdbapi.com/?apikey=75522b56&i="
 
   constructor(public http: HttpClient) {
     console.log('Hello RestProvider Provider');
   }
 
+  searchMovieByKey(keyword : string) {
+    return new Promise(resolve => {
+      this.http.get(this.apiSearchMovieUrl+keyword).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  searchSerieByKey(keyword : string) {
+    return new Promise(resolve => {
+      this.http.get(this.apiSearchSerieUrl+keyword).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getMediaById(id : string) {
+    return new Promise(resolve => {
+      this.http.get(this.apiGetMediaUrl+id).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getSeasonByNumber(id : string, season : number) {
+    return new Promise(resolve => {
+      this.http.get(this.apiGetMediaUrl+id+"&Season="+season).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+  getEpisodeByNumber(id : string, season : number, episode : number) {
+    return new Promise(resolve => {
+      this.http.get(this.apiGetMediaUrl+id+"&Season="+season+"&Episode="+episode).subscribe(data => {
+        resolve(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
 }
