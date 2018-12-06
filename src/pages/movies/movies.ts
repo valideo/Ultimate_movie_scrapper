50e73abd63c804db412c4041e8ad283d1e9f9acd
valideo <valentin.bredy@hotmail.fr>
2018-12-06 20:53:08 +0100

    added infinite scrolling search

--- src/pages/movies/movies.ts
+++ src/pages/movies/movies.ts
@@ -11,22 +11,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MoviesPage {
 
   items: any = [];
+  newData: any = [];
+  page: number = 1;
+  keywords: string = "";
+  notAllLoaded: boolean = true;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public RestProvider: RestProvider) {
   }
 
   getMovies(key : string) {
-    this.RestProvider.searchMovieByKey(key)
+    this.RestProvider.searchMovieByKey(key, this.page.toString())
     .then(data => {
       this.items = data;
       console.log(this.items);
     });
   }
 
+  doInfinite(): Promise<any> {
+    console.log('Begin async operation');
+    this.page +=1;
+    return new Promise((resolve) => {
+      setTimeout(() => {
+    this.RestProvider.searchMovieByKey(this.keywords, this.page.toString())
+    .then(data => {
+     this.newData = data;
+      console.log(this.newData);
+      console.log(this.page);
+      for (var i = 0; i < this.newData.length ; i++) {
+        this.items.push( this.newData[i] );
+        if(this.newData.length < 10 && i == this.newData.length -1){
+          this.notAllLoaded = false;
+        }
+      }
+    });
+        console.log('Async operation has ended');
+        resolve();
+      }, 500);
+    })
+  }
+
   searchMovie(ev: any) {
-    const val = ev.target.value;
-    if (val && val.trim() != '') {
-      this.getMovies(val);
+    this.notAllLoaded = true;
+    this.keywords = ev.target.value;
+    if (this.keywords && this.keywords.trim() != '') {
+      this.getMovies(this.keywords);
     }
 
   }
